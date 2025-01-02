f6086ac931e141ba517473f973f529d52e74b36f
Bbee7 <2221674709@qq.com>
2025-01-03 00:23:28 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Bbee7.github.io/' // 替换为您的 GitHub 仓库名称
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Bbee7.github.io/' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: false,
